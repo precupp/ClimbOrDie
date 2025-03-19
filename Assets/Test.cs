c7ea6347172256f44701530925940016485ab830
precup <davidprecup2@gmail.com>
2025-03-19 14:24:55 +0100

    Unity Setup

---
+++ Assets/Test.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Test, Can u  Git Pull?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
