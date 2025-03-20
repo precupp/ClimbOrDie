3adf6fca43d5311f571eb05b758b47dde73fb326
precup <davidprecup2@gmail.com>
2025-03-20 10:47:03 +0100

    Test Update

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -5,7 +5,7 @@ public class Test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Test, Can u  Git Pull? 123 hallo
+        // Test, Can u  Git Pull? 123 hallo Testq23456y7treswdfvg
     }
 
     // Update is called once per frame
